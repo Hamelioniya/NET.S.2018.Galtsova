db506c7790a9eae62098a507c1a5e412a4f6fcc1
Anastasiya <galtsova98@gmail.com>
2018-03-14 20:07:42 +0300

    Add PokemonLibrary.dll to the GAC

---
+++ NET.W.2018.Galtsova.01/PokemonLibrary/Pokemon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class Pokemon
+    {
+        public string pokemonName { get; set; }
+        public string pokemonType { get; set; }
+        public string pokemonDescription { get; set; }
+
+        public Pokemon(string pokemonName, string pokemonType, string pokemonDescription)
+        {
+            this.pokemonName = pokemonName;
+            this.pokemonType = pokemonType;
+            this.pokemonDescription = pokemonDescription;
+        }
+    }
+}
---
+++ NET.W.2018.Galtsova.01/PokemonLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PokemonLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PokemonLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c537e5d2-b135-4796-86d0-8c76de6940b9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
