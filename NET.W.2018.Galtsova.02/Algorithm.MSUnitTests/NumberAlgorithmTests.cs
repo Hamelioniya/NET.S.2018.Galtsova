11665aad89914959a47906d220fe935ba0ba4819
Anastasiya <galtsova98@gmail.com>
2018-03-15 14:33:58 +0300

    Add MS unit tests for the InsertNumber method

---
+++ NET.W.2018.Galtsova.02/Algorithm.MSUnitTests/NumberAlgorithmTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+
+namespace Algorithm.MSUnitTests
+{
+    [TestClass]
+    public class NumberAlgorithmTests
+    {
+        [TestMethod]
+        public void InsertNumber_NumberInsert15and15From0To0Test()
+        {
+            int numberSource = 15, numberIn = 15;
+            int firstIndex = 0, lastIndex = 0;
+            int expected = 15;
+
+            int result = NumberAlgorithm.InsertNumber(numberSource, numberIn, firstIndex, lastIndex);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void InsertNumber_NumberInsert8and15From0To0Test()
+        {
+            int numberSource = 8, numberIn = 15;
+            int firstIndex = 0, lastIndex = 0;
+            int expected = 9;
+
+            int result = NumberAlgorithm.InsertNumber(numberSource, numberIn, firstIndex, lastIndex);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void InsertNumber_NumberInsert8and15From3To8Test()
+        {
+            int numberSource = 8, numberIn = 15;
+            int firstIndex = 3, lastIndex = 8;
+            int expected = 120;
+
+            int result = NumberAlgorithm.InsertNumber(numberSource, numberIn, firstIndex, lastIndex);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InsertNumber_ArgumentExceptionFirstIndexGreaterThanLastTest()
+        {
+            int numberSource = 15, numberIn = 15;
+            int firstIndex = 2, lastIndex = 1;
+
+            NumberAlgorithm.InsertNumber(numberSource, numberIn, firstIndex, lastIndex);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InsertNumber_ArgumentExceptionIndexLessThan0Test()
+        {
+            int numberSource = 15, numberIn = 15;
+            int firstIndex = -1, lastIndex = 0;
+
+            NumberAlgorithm.InsertNumber(numberSource, numberIn, firstIndex, lastIndex);
+        }
+    }
+}
